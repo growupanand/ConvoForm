975ae1137f6c089a01b3b60c8128b57a48adc815
Utkarsh Anand <growupanand@gmail.com>
2023-11-25 03:38:23 +0530

    [frontend] Added register page

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/auth/register',
+                permanent: false,
+            },
+        ]
+    },
+}
 
 module.exports = nextConfig
---
+++ src/app/auth/layout.tsx
@@ -0,0 +1,18 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function Layout({ children }: Readonly<Props>) {
+  return (
+    <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
+      <Card className="shadow-md bg-white  rounded-md border min-w-[400px]">
+        <CardHeader>
+          <CardTitle className=" text-center">Smart form wizard</CardTitle>
+        </CardHeader>
+        <CardContent>{children}</CardContent>
+      </Card>
+    </div>
+  );
+}
