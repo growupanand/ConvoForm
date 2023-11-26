e098d349fff170aef1ffdfd14dc0ba5702473666
Utkarsh Anand <growupanand@gmail.com>
2023-11-27 01:16:28 +0530

    Added API - get workspaces

--- src/app/api/workspaces/route.ts
+++ src/app/api/workspaces/route.ts
@@ -6,26 +6,46 @@ import { z } from "zod";
 
 const prisma = new PrismaClient();
 
-
 const workspaceCreateSchema = z.object({
-    name: z.string().min(1).max(255),
-})
+  name: z.string().min(1).max(255),
+});
 
-export async function POST(req:Request) {
-    try {const user = await getCurrentUser();
-        const requestJson = await req.json();
+export async function POST(req: Request) {
+  try {
+    const user = await getCurrentUser();
+    const requestJson = await req.json();
     const reqPayload = workspaceCreateSchema.parse(requestJson);
     const newWorkspace = await prisma.workspace.create({
-        data: {
-            name: reqPayload.name,
-            userId: user.id,
-        }});
-    return NextResponse.json(newWorkspace, {status: 201});}
-
-    catch (error) {
-        if (error instanceof z.ZodError) {
-            return NextResponse.json(error.issues, {status: 400})
-        }
-        return NextResponse.json({nonFieldError : defaultErrorMessage}, {status: 500})
+      data: {
+        name: reqPayload.name,
+        userId: user.id,
+      },
+    });
+    return NextResponse.json(newWorkspace, { status: 201 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(error.issues, { status: 400 });
     }
+    return NextResponse.json(
+      { nonFieldError: defaultErrorMessage },
+      { status: 500 }
+    );
+  }
+}
+
+export async function GET(req: Request) {
+  try {
+    const user = await getCurrentUser();
+    const workspaces = await prisma.workspace.findMany({
+      where: {
+        userId: user.id,
+      },
+    });
+    return NextResponse.json(workspaces);
+  } catch (error) {
+    return NextResponse.json(
+      { nonFieldError: defaultErrorMessage },
+      { status: 500 }
+    );
+  }
 }
