a5afd159913aca6648cf3378a2693d253bdff5fa
Utkarsh Anand <growupanand@gmail.com>
2023-11-26 22:33:53 +0530

    As user I can, create workspaces #7

--- src/app/dashboard/workspaces/page.tsx
+++ src/app/dashboard/workspaces/page.tsx
@@ -1,3 +1,12 @@
+import CreateWorkspaceButton from "@/components/dashboard/workspaces/createWorkspaceButton";
+
 export default function WorkspacesPage() {
-  return <div>this is workspace page</div>;
+  return (
+    <div>
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl">Workspaces</h1>
+        <CreateWorkspaceButton />
+      </div>
+    </div>
+  );
 }
---
+++ src/lib/fetch.ts
@@ -0,0 +1,22 @@
+
+type ApiClientOptions = RequestInit & {
+    data? : Record<string, any>
+}
+
+type ApiClient = (url:NodeJS.fetch.RequestInfo, fetchOptions:ApiClientOptions) => Promise<Response>;
+
+export const apiClient:ApiClient = async (url, fetchOptions) => {
+    const {data, ...otherFetchOptions} = fetchOptions;
+    const response = await fetch(url, {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        ...(data ? {body: JSON.stringify(data)} : {}),
+        ...otherFetchOptions,
+    });
+
+    if (!response.ok) {
+        throw response;
+    }
+    return response;
+}
