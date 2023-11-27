20677121d1b18ca81583def08c7083e8e24a1e51
Utkarsh Anand <growupanand@gmail.com>
2023-11-27 21:17:25 +0530

    Minor changes

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -1,33 +1,27 @@
 import { NextAuthOptions } from "next-auth";
 import { env } from "./env";
-import { PrismaClient } from "@prisma/client";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-
-
-const prisma = new PrismaClient();
-
+import { db } from "./db";
 
 export const authOptions: NextAuthOptions = {
-    pages: {
-      signIn: "/login",
-    },
-    adapter: PrismaAdapter(prisma),
-  
-    providers: [
-      GoogleProvider({
-        clientId: env.GOOGLE_CLIENT_ID,
-        clientSecret: env.GOOGLE_CLIENT_SECRET,
-      }),
-      // ...add more providers here
-    ],
-    callbacks: {
-      // Add userId to session
-      async session({  session, user }) {
-        session.user.id = user.id;
-        return session
-      },
-    },
+  pages: {
+    signIn: "/login",
+  },
+  adapter: PrismaAdapter(db),
 
-  
-  };
+  providers: [
+    GoogleProvider({
+      clientId: env.GOOGLE_CLIENT_ID,
+      clientSecret: env.GOOGLE_CLIENT_SECRET,
+    }),
+    // ...add more providers here
+  ],
+  callbacks: {
+    // Add userId to session
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    },
+  },
+};
