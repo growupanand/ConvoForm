4306a0ed3df74c8188343e2ce6db9c44445d44cd
UTKARSH ANAND <growupanand@gmail.com>
2023-12-24 18:44:23 +0530

    Revert "Added email provider to login using magic links"
    This reverts commit fa9932cab25dfc22a8d2d062da4e7c81bfa1362f.

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import Link from "next/link";
 
 type Props = {
   children: React.ReactNode;
@@ -9,10 +8,8 @@ export default function AuthLayout({ children }: Readonly<Props>) {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-gray-100">
       <Card className="shadow-md bg-white flex flex-col rounded-md border md:min-w-[400px] min-h-[400px]">
-        <CardHeader className="mb-5">
-          <Link href="/">
-            <CardTitle className=" text-center">Smart form wizard</CardTitle>
-          </Link>
+        <CardHeader>
+          <CardTitle className=" text-center">Smart form wizard</CardTitle>
         </CardHeader>
         <CardContent className="grow">{children}</CardContent>
       </Card>
