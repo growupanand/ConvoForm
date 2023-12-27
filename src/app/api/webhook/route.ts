f67bc6f5f26548d91caf12c68f41caa2a82d90a3
Utkarsh Anand <growupanand@gmail.com>
2023-12-27 23:11:03 +0530

    Added webhook handler for user deleted event

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/db";
 import type { WebhookEvent } from "@clerk/clerk-sdk-node";
+import { Organization, OrganizationMember } from "@prisma/client";
 
 export async function POST(req: NextRequest) {
   const reqJson = await req.json();
@@ -32,6 +33,74 @@ export async function POST(req: NextRequest) {
         console.error({
           message: "Unable to create user",
           data: data,
+          errorMessage: e.message,
+        });
+      }
+      break;
+    case "user.deleted":
+      console.log("user deleted event received");
+      const { data: userData } = event;
+      try {
+        if (!userData.id) {
+          console.log("user id not found in event data");
+          break;
+        }
+        await prisma.user.deleteMany({
+          where: {
+            userId: userData.id,
+          },
+        });
+        console.log("user deleted successfully in our database");
+      } catch (e) {
+        console.error({
+          message: "Unable to delete user",
+          data: userData,
+          errorMessage: e.message,
+        });
+      }
+      break;
+
+    case "organization.created":
+      console.log("organization created event received");
+      const { data: orgData } = event;
+      try {
+        const org = {
+          name: orgData.name,
+          organizationId: orgData.id,
+          slug: orgData.slug || "",
+        };
+        await prisma.organization.create({
+          data: org,
+        });
+        console.log("organization created successfully in our database");
+      } catch (e) {
+        console.error({
+          message: "Unable to create organization",
+          data: orgData,
+          errorMessage: e.message,
+        });
+      }
+      break;
+    case "organizationMembership.created":
+      console.log("organizationMembership created event received");
+      const { data: orgMembershipData } = event;
+      try {
+        const orgMembership = {
+          memberId: orgMembershipData.id,
+          organizationId: orgMembershipData.organization.id,
+          userId: orgMembershipData.public_user_data.user_id,
+          role: orgMembershipData.role,
+        };
+        await prisma.organizationMember.create({
+          data: orgMembership,
+        });
+        console.log(
+          "organizationMembership created successfully in our database"
+        );
+      } catch (e) {
+        console.error({
+          message: "Unable to create organizationMembership",
+          data: orgMembershipData,
           errorMessage: e.message,
         });
       }
