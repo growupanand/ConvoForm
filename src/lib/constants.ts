29876d15b9c30b2ba07d1d0098136b2277c81bee
Utkarsh Anand <growupanand@gmail.com>
2024-01-11 14:38:22 +0530

    The first question should not always starts with 'Great! I can help you with that'.

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,6 @@ export const defaultErrorMessage = "Something went wrong.";
 export const OPEN_AI_MODEL = process.env.OPEN_AI_MODEL ?? "gpt-3.5-turbo-1106";
 export const OPENAI_API_KEY = process.env.OPENAI_API_KEY ?? "";
 
-export const CONVERSATION_START_MESSAGE = "Hello, I want to fill the form";
+export const CONVERSATION_START_MESSAGE = "Hi";
 
 export const brandName = "Convo Form";
