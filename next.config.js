5e68a6564c1496a9c4aeb0e7ef6d75babce999f9
Utkarsh Anand <growupanand@gmail.com>
2024-01-13 12:24:07 +0530

    Fixed build error related to sentry

--- next.config.js
+++ next.config.js
@@ -31,7 +31,7 @@ module.exports = withSentryConfig(
     // Suppresses source map uploading logs during build
     silent: true,
     org: "growupanand",
-    project: "convo-form",
+    project: "convoform",
   },
   {
     // For all available options, see:
