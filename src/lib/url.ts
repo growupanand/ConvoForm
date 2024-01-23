f738b6220b36f775a7a187322d75cb84305b9fa0
Utkarsh Anand <growupanand@gmail.com>
2024-01-23 12:17:22 +0530

    fix: 🐛 wrong domain name on production doployment
    Added NEXT_PUBLIC_APP_URL environment to use on production deployment
    
    ✅ Closes: #159

--- src/lib/url.ts
+++ src/lib/url.ts
@@ -1,21 +1,25 @@
+export const getFrontendBaseUrl = () => {
+  // If App url is defined (Production), return it
+  const appUrl = process.env.NEXT_PUBLIC_APP_URL;
+  if (appUrl) {
+    return appUrl;
+  }
+
+  // Else if it is vercel deployment, return the preview url
+  const vc = process.env.VERCEL_URL;
+  if (vc) {
+    return `https://${vc}`;
+  }
+
+  // else return localhost url
+  return `http://localhost:3000`;
+};
+
 export const getBackendBaseUrl = () => {
   // If this is browser environment, return empty string
   // because we want to use relative path for browser
   if (typeof window !== "undefined") return "";
 
-  // If this is vercel environment, return the preview url
-  const vc = process.env.VERCEL_URL;
-  if (vc) return `https://${vc}`;
-
-  // If this is local environment, return localhost url
-  return `http://localhost:3000`;
-};
-
-export const getFrontendBaseUrl = () => {
-  // If this is vercel environment, return the preview url
-  const vc = process.env.VERCEL_URL;
-  if (vc) return `https://${vc}`;
-
-  // If this is local environment, return localhost url
-  return `http://localhost:3000`;
+  // else return frontend url as we are using nextjs api routes
+  return getFrontendBaseUrl();
 };
