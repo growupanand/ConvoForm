630da228ec083d936268517378ea5b54ca438373
Utkarsh Anand <growupanand@gmail.com>
2024-01-25 01:08:15 +0530

    feat: 🚀 Added charts in dashbard page
    Added charts in dashboard total count page, to show forms created and
    form response collected count of daily basis on bar graph
    
    ✅ Closes: #147

--- src/app/(privatePage)/(mainPage)/dashboard/page.tsx
+++ src/app/(privatePage)/(mainPage)/dashboard/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "Dashboard",
 };
 
-const getFormsCount = async (organizationId: string) => {
+const getTotalFormsCount = async (organizationId: string) => {
   return prisma.form.count({
     where: {
       organizationId: organizationId,
@@ -17,10 +17,18 @@ const getFormsCount = async (organizationId: string) => {
   });
 };
 
-const getLastForm = async (organizationId: string) => {
-  return prisma.form.findFirst({
+const getLastCreatedForms = async (
+  organizationId: string,
+  lastDaysCount: number,
+) => {
+  const lastDaysDate = new Date();
+  lastDaysDate.setDate(lastDaysDate.getDate() - lastDaysCount);
+  return prisma.form.findMany({
     where: {
       organizationId: organizationId,
+      createdAt: {
+        gte: lastDaysDate,
+      },
     },
     orderBy: {
       createdAt: "desc",
@@ -31,7 +39,7 @@ const getLastForm = async (organizationId: string) => {
   });
 };
 
-const getConversationsCount = async (organizationId: string) => {
+const getTotalConversationsCount = async (organizationId: string) => {
   return prisma.conversation.count({
     where: {
       organizationId: organizationId,
@@ -39,10 +47,18 @@ const getConversationsCount = async (organizationId: string) => {
   });
 };
 
-const getLastConversation = async (organizationId: string) => {
-  return prisma.conversation.findFirst({
+const getLastCreatedConversations = async (
+  organizationId: string,
+  lastDaysCount: number,
+) => {
+  const lastDaysDate = new Date();
+  lastDaysDate.setDate(lastDaysDate.getDate() - lastDaysCount);
+  return prisma.conversation.findMany({
     where: {
       organizationId: organizationId,
+      createdAt: {
+        gte: lastDaysDate,
+      },
     },
     orderBy: {
       createdAt: "desc",
@@ -56,41 +72,96 @@ const getLastConversation = async (organizationId: string) => {
 export default async function DashboardPage() {
   const orgId = getOrganizationId();
 
-  const formsCount = await getFormsCount(orgId);
-  let lastFormCreatedAt = null as string | null;
-  if (formsCount > 0) {
-    const lastForm = await getLastForm(orgId);
+  const currentMonthTotalDays = new Date(
+    new Date().getFullYear(),
+    new Date().getMonth() + 1,
+    0,
+  ).getDate();
+  // get array of day of lastDaysLength days,
+  // E.g if today is 24 then [24, 23, 22, 21, 20, 19, 18]
+  const lastDays = Array.from(
+    { length: currentMonthTotalDays },
+    (_, i) => currentMonthTotalDays - i,
+  );
+
+  const formsTotalCount = await getTotalFormsCount(orgId);
+  const lastCreatedForms = await getLastCreatedForms(
+    orgId,
+    currentMonthTotalDays,
+  );
+  let lastFormCreatedAt = null;
+  if (lastCreatedForms.length > 0) {
+    const lastForm = lastCreatedForms[0];
     lastFormCreatedAt = lastForm ? timeAgo(lastForm.createdAt) : null;
   }
+  const formCountsDataDayWise = {} as Record<string, any>;
+  lastDays.forEach((day) => {
+    formCountsDataDayWise[day] = 0;
+  });
+  for (let i = 0; i < lastCreatedForms.length; i++) {
+    const form = lastCreatedForms[i];
+    const formCreatedAtDay = form.createdAt.getDate();
+    formCountsDataDayWise[formCreatedAtDay] += 1;
+  }
+  const formCountDataArray = Object.entries(formCountsDataDayWise).map(
+    ([name, value]) => ({
+      name,
+      count: value,
+    }),
+  );
 
-  const conversationsCount = await getConversationsCount(orgId);
-  let lastConversationCreatedAt = null as string | null;
-  if (conversationsCount > 0) {
-    const lastConversation = await getLastConversation(orgId);
+  const totalConversationsCount = await getTotalConversationsCount(orgId);
+  const lastCreatedConversations = await getLastCreatedConversations(
+    orgId,
+    currentMonthTotalDays,
+  );
+  let lastConversationCreatedAt = null;
+  if (lastCreatedConversations.length > 0) {
+    const lastConversation = lastCreatedConversations[0];
     lastConversationCreatedAt = lastConversation
       ? timeAgo(lastConversation.createdAt)
       : null;
   }
 
+  const conversationsCountDataDayWise = {} as Record<string, any>;
+  lastDays.forEach((day) => {
+    conversationsCountDataDayWise[day] = 0;
+  });
+  for (let i = 0; i < lastCreatedConversations.length; i++) {
+    const conversation = lastCreatedConversations[i];
+    const conversationCreatedAtDay = conversation.createdAt.getDate();
+    conversationsCountDataDayWise[conversationCreatedAtDay] += 1;
+  }
+  const conversationCountDataArray = Object.entries(
+    conversationsCountDataDayWise,
+  ).map(([name, value]) => ({
+    name,
+    count: value,
+  }));
+
   return (
-    <div>
+    <div className="max-lg:p-3">
       <h1 className="mb-5 py-3 text-xl font-medium lg:text-2xl">Dashboard</h1>
-      <div className="grid gap-3 lg:grid-cols-4">
+      <div className="grid gap-3 lg:grid-flow-col">
         <DataCard
           title="Forms created"
-          mainValue={formsCount.toString()}
+          mainValue={formsTotalCount.toString()}
           secondaryValue={
             lastFormCreatedAt ? `Last ${lastFormCreatedAt}` : undefined
           }
+          dataType="Total"
+          chartData={formCountDataArray}
         />
         <DataCard
           title="Response collected"
-          mainValue={conversationsCount.toString()}
+          mainValue={totalConversationsCount.toString()}
           secondaryValue={
             lastConversationCreatedAt
               ? `Last ${lastConversationCreatedAt}`
               : undefined
           }
+          dataType="Total"
+          chartData={conversationCountDataArray}
         />
       </div>
     </div>
