77333cb63c1fc57454dc603b46b20b56ffba62fd
Utkarsh Anand <growupanand@gmail.com>
2024-01-27 14:04:25 +0530

    perf: ⚡️ added sperate organization selection page
    Created seperate page for selecting and creating organiation, this will
    allow us to prevent any  page break  where no organiization is cretated
    
    ✅ Closes: #169

--- apps/web/src/lib/getOrganizationId.ts
+++ apps/web/src/lib/getOrganizationId.ts
@@ -4,7 +4,7 @@ import { auth } from "@clerk/nextjs";
 export const getOrganizationId = () => {
   const { orgId } = auth();
   if (!orgId) {
-    redirect("/");
+    redirect("/organizations");
   }
   return orgId;
 };
