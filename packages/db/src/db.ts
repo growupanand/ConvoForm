109039b4e12f6e8bd454cd3e6b6aeaae6faa844f
UTKARSH ANAND <growupanand@gmail.com>
2024-02-10 02:17:32 +0530

    Revert "perf: ⚡️ converted tRPC api route into edge runtime"
    This reverts commit 2fe96acc8cbdc555df6e468d49e3716b3ad06922.

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,5 +1,5 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import * as schema from "./schema";
 
