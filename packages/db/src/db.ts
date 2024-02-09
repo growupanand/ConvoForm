c43addeec56bbf46d25ab8b46b9eb384059a73bd
Utkarsh Anand <growupanand@gmail.com>
2024-02-10 02:07:52 +0530

    perf: ⚡️ converted tRPC api route into edge runtime
    ✅ Closes: #182

--- packages/db/src/db.ts
+++ packages/db/src/db.ts
@@ -1,5 +1,5 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import { Pool } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 
 import * as schema from "./schema";
 
