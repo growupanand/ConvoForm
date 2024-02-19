db04ffcefdc9729e7b01ea7080aef38518b8a8b2
Utkarsh Anand <growupanand@gmail.com>
2024-02-19 22:53:58 +0530

    style: 💄 make generate AI form dialog responsive

--- apps/web/src/app/(protectedPage)/(mainPage)/workspaces/[workspaceId]/_component/createFormButton.tsx
+++ apps/web/src/app/(protectedPage)/(mainPage)/workspaces/[workspaceId]/_component/createFormButton.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { Workspace } from "@convoform/db";
 import { Button } from "@convoform/ui/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@convoform/ui/components/ui/dropdown-menu";
 import { toast } from "@convoform/ui/components/ui/use-toast";
-import { Loader2, Plus } from "lucide-react";
+import { Loader2, PenLine, Plus, Sparkles } from "lucide-react";
 import { z } from "zod";
 
 import { montserrat } from "@/app/fonts";
@@ -31,6 +39,8 @@ const newFormData = createFormSchema.parse({
 });
 
 export default function CreateFormButton({ workspace }: Readonly<Props>) {
+  const [open, setOpen] = useState(false);
+
   const router = useRouter();
   const createForm = api.form.create.useMutation({
     onSuccess: async (newForm) => {
@@ -61,19 +71,56 @@ export default function CreateFormButton({ workspace }: Readonly<Props>) {
   return (
     <div>
       <div className="mb-5 flex items-center justify-start gap-3">
-        <Button
-          variant="secondary"
-          disabled={isCreatingForm}
-          onClick={() => handleCreateForm(newFormData)}
-          className={cn(montserrat.className, "font-semibold")}
-        >
-          {isCreatingForm && <Loader2 className="mr-2 h-4 w-4 animate-spin" />}
-          {!isCreatingForm && <Plus className="mr-2 h-4 w-4" />}
-          New Form
-        </Button>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="secondary"
+              className={cn(
+                montserrat.className,
+                "cursor-pointer font-semibold",
+              )}
+            >
+              {isCreatingForm && (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              )}
+              {!isCreatingForm && <Plus className="mr-2 h-4 w-4" />}
+              <span>New Form</span>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className=" p-3" align="start">
+            <DropdownMenuGroup className="grid gap-2">
+              <DropdownMenuItem
+                className={cn(
+                  montserrat.className,
+                  "cursor-pointer px-3 font-semibold",
+                )}
+                disabled={isCreatingForm}
+                onClick={() => handleCreateForm(newFormData)}
+              >
+                <PenLine size={16} className="mr-2" />
+                <span>Create form Scratch</span>
+              </DropdownMenuItem>
+              <DropdownMenuItem
+                disabled={isCreatingForm}
+                className={cn(
+                  montserrat.className,
+                  "cursor-pointer px-3 font-semibold",
+                )}
+                onClick={() => setOpen(true)}
+              >
+                <Sparkles size={16} className="mr-2" />
+
+                <span>Generate using AI</span>
+              </DropdownMenuItem>
+            </DropdownMenuGroup>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <GenerateForm
           isCreatingForm={isCreatingForm}
           onFormGenerated={handleCreateForm}
+          open={open}
+          setOpen={setOpen}
         />
       </div>
     </div>
