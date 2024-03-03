386eac7ae6a1767f6f7dc805df5b2aff7dc3681f
Utkarsh Anand <growupanand@gmail.com>
2024-03-03 22:44:48 +0530

    fix: 🐛 Sometimes form editor page not loading
    Moved clerk provider to root layout, and make root page force static
    rendered
    
    ✅ Closes: #206

--- apps/web/src/app/(protectedPage)/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthProvider } from "@/components/providers/authProvider";
-
-type Props = {
-  children: React.ReactNode;
-};
-
-export default function Layout({ children }: Props) {
-  return <AuthProvider>{children}</AuthProvider>;
-}
