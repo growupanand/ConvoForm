27af2f1a24610559809fe6da5c1f5309103815ed
Utkarsh Anand <growupanand@gmail.com>
2024-07-20 13:11:35 +0530

    perf: ⚡️ Multi choice answer will save exact value WO validation
    When user will click on choice button to answer the question, this
    answer will not be validate by AI, instead will be saved exact value in
    database
    
    ✅ Closes: 331

--- packages/db/src/schema/formFields/constants.ts
+++ packages/db/src/schema/formFields/constants.ts
@@ -6,16 +6,23 @@ export const inputTypeEnum = pgEnum("inputTypeEnum", INPUT_TYPES);
 
 type InputTypeMap = Record<
   (typeof INPUT_TYPES)[number],
-  { name: string; description: string }
+  {
+    name: string;
+    description: string;
+    /** Should skip answer validation and save exact value in the database */
+    saveExactValue: boolean;
+  }
 >;
 
 export const INPUT_TYPES_MAP: InputTypeMap = {
   text: {
     name: "Text",
     description: "Respondent can type an answer",
+    saveExactValue: false,
   },
   multipleChoice: {
     name: "Multiple Choice",
     description: "Respondent can select an answer from a list of choices",
+    saveExactValue: true,
   },
 };
