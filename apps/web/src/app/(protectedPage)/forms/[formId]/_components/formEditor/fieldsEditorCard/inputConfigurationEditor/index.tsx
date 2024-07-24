2ea0bd6c5e420159a6861d7e6bedd2f071505deb
Utkarsh Anand <growupanand@gmail.com>
2024-07-24 23:39:01 +0530

    feat: 🚀 Added DatePicker Input configuration in field editor
    As form creator, I can select Date picker input type in edit field sheet
    
    ✅ Closes: 334

--- apps/web/src/app/(protectedPage)/forms/[formId]/_components/formEditor/fieldsEditorCard/inputConfigurationEditor/index.tsx
+++ apps/web/src/app/(protectedPage)/forms/[formId]/_components/formEditor/fieldsEditorCard/inputConfigurationEditor/index.tsx
@@ -1,7 +1,9 @@
 import type { InputTypeSchema } from "@convoform/db/src/schema";
 import type { UseFormReturn } from "react-hook-form";
 
+import { MutedText } from "@convoform/ui/components/ui/mutedText";
 import type { FormHookData } from "../editFieldSheet";
+import { DatePickerInputConfiguration } from "./datePickerInputConfiguration";
 import { MultiChoiceInputConfiguration } from "./multiChoiceInputConfiguration";
 import { TextInputConfigurationEditor } from "./textInputConfigurationEditor";
 
@@ -14,12 +16,32 @@ export function InputConfigurationEditor({
   inputType,
   formHook,
 }: Readonly<Props>) {
-  switch (inputType) {
-    case "text":
-      return <TextInputConfigurationEditor formHook={formHook} />;
-    case "multipleChoice":
-      return <MultiChoiceInputConfiguration formHook={formHook} />;
-    default:
-      return null;
-  }
+  const renderInputConfiguration = () => {
+    switch (inputType) {
+      case "text":
+        return <TextInputConfigurationEditor formHook={formHook} />;
+      case "multipleChoice":
+        return <MultiChoiceInputConfiguration formHook={formHook} />;
+      case "datePicker":
+        return <DatePickerInputConfiguration formHook={formHook} />;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <InputConfigurationEditorWraper>
+      {renderInputConfiguration()}
+    </InputConfigurationEditorWraper>
+  );
+}
+
+function InputConfigurationEditorWraper({
+  children,
+}: { children: React.ReactNode }) {
+  return <div className="grid space-y-4 pt-4">{children}</div>;
+}
+
+export function OptionalText() {
+  return <MutedText>(optional)</MutedText>;
 }
