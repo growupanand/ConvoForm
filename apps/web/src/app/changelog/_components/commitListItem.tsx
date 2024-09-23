9540f86f7a3c2233ee5596539e8ab9e06dfc9ef2
Utkarsh Anand <growupanand@gmail.com>
2024-09-23 22:57:47 +0530

    style: 💄 changelog page have new UI

---
+++ apps/web/src/app/changelog/_components/commitListItem.tsx
@@ -0,0 +1,32 @@
+import { Dot } from "lucide-react";
+import Link from "next/link";
+
+import { extractCommitMessage } from "@/lib/github";
+import type { Commit } from "@/lib/validations/changeLog";
+import { Button } from "@convoform/ui/components/ui/button";
+
+export const CommitItem = ({ commit }: { commit: Commit }) => {
+  const commitMessage = extractCommitMessage(commit.message);
+
+  return (
+    <div className="flex items-center">
+      <div className="">
+        <Dot className="size-6" />
+      </div>
+      <Button
+        variant="link"
+        size="sm"
+        className="capitalize text-wrap h-auto min-h-9"
+        asChild
+      >
+        <Link
+          href={commit.href}
+          target="_blank"
+          rel="noopener noreferrer nofollow"
+        >
+          {commitMessage}
+        </Link>
+      </Button>
+    </div>
+  );
+};
--- apps/web/src/components/requestFeatureCard.tsx
+++ apps/web/src/components/requestFeatureCard.tsx
@@ -1,35 +1,18 @@
 import { Button } from "@convoform/ui/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-} from "@convoform/ui/components/ui/card";
-import { ExternalLink } from "lucide-react";
+
 import Link from "next/link";
 
 export function RequestFeatureCard() {
   return (
-    <Card>
-      <CardContent className="pt-6">
-        <CardDescription className="text-lg">
-          {`Feel like something's missing?, just post any feature request or
-            idea here, and we will notify you once they are ready.`}
-        </CardDescription>
-      </CardContent>
-      <CardFooter>
-        <div className="flex justify-end ">
-          <Button asChild variant="secondary">
-            <Link
-              target="_blank"
-              href="https://convoform.canny.io/feature-requests"
-            >
-              Request Feature
-              <ExternalLink className="ml-2 h-4 w-4" />
-            </Link>
-          </Button>
-        </div>
-      </CardFooter>
-    </Card>
+    <div className="flex items-center py-6">
+      <Button asChild variant="default" size="lg">
+        <Link
+          target="_blank"
+          href="https://convoform.canny.io/feature-requests"
+        >
+          Request new feature
+        </Link>
+      </Button>
+    </div>
   );
 }
