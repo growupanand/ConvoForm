26247b915eece6618f290aad943bad407ef5462d
git-workflow-prepare-release <growupanand@gmail.com>
2024-09-25 15:28:44 +0000

    release: changelog for release note v0.14.0

--- packages/release/src/releases/index.ts
+++ packages/release/src/releases/index.ts
@@ -1,1 +1,5 @@
-export const releaseMDXComponents = {};
+import { default as Release_0_14_0 } from "./release_0_14_0.mdx";
+
+export const releaseMDXComponents = {
+  Release_0_14_0: Release_0_14_0,
+};
