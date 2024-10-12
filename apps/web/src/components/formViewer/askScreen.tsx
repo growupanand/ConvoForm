c08790434ba71052beec02101684e76e533dc67a
Utkarsh Anand <growupanand@gmail.com>
2024-10-12 12:46:50 +0530

    style: 💄 improve typing effect while showing question
    ✅ Closes: 149

--- apps/web/src/components/formViewer/askScreen.tsx
+++ apps/web/src/components/formViewer/askScreen.tsx
@@ -5,8 +5,8 @@ import type {
   FormDesignRenderSchema,
 } from "@convoform/db/src/schema";
 import type { SubmitAnswer } from "@convoform/react";
-import { Tally1 } from "lucide-react";
 
+import { TypingEffect } from "../common/typingEffect";
 import { DynamicAnswerInput } from "./DynamicAnswerInput";
 
 type Props = {
@@ -26,17 +26,14 @@ export const AskScreen = ({
 }: Props) => {
   return (
     <div className="flex min-h-full w-full flex-col justify-center px-3 ">
-      <div className="text-muted-foreground mb-4 text-lg font-normal capitalize">
+      <div className="text-muted-foreground mb-4 text-lg capitalize">
         {currentField?.fieldName}
       </div>
       <h1
         style={{ color: formDesign.fontColor }}
         className="mb-10 w-full whitespace-break-spaces text-justify text-xl leading-6 lg:text-2xl lg:leading-7 transition-colors duration-500"
       >
-        <span>
-          {currentQuestion}
-          {isFormBusy && <Tally1 className="ml-2 inline animate-ping" />}
-        </span>
+        <TypingEffect text={currentQuestion} />
       </h1>
 
       {!isFormBusy && currentField && (
