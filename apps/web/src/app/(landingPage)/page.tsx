44deea22f889b9ddd25f8d97f04ca0f31f0ac01c
Utkarsh Anand <growupanand@gmail.com>
2024-11-05 18:47:06 +0530

    style: 💄 added demo section in landing page
    Demo section will show an live demo form, it will render only on desktop screen
    
    ✅ Closes: 363

--- apps/web/src/app/(landingPage)/page.tsx
+++ apps/web/src/app/(landingPage)/page.tsx
@@ -2,37 +2,58 @@ import { Features } from "@/app/(landingPage)/_components/features";
 import { Hero } from "@/app/(landingPage)/_components/hero";
 import { HowToUseSection } from "@/app/(landingPage)/_components/howToUse";
 import { Pricing } from "@/app/(landingPage)/_components/pricing";
+import BrowserWindow from "@/components/common/browserWindow";
+import { getFrontendBaseUrl } from "@/lib/url";
+import dynamic from "next/dynamic";
 import Image from "next/image";
 import Link from "next/link";
-// import { NewFeature } from "./_components/newFeature";
+import { DEMO_FORM_ID } from "./_components/constants";
+import { DemoCardSkeleton } from "./_components/demoCard";
 import { ScreenshotSlider } from "./_components/screenshotsSlider";
-import { SectionShell } from "./_components/sectionShell";
+import { SectionContainer } from "./_components/sectionShell";
+
+const LazyDemoCard = dynamic(
+  () => import("./_components/demoCard").then((mod) => mod.DemoCard),
+  {
+    ssr: false,
+    loading: () => <DemoCardSkeleton />,
+  },
+);
 
 export default function Home() {
+  const demoFormLink = `${getFrontendBaseUrl()}/view/${DEMO_FORM_ID}`;
+
   return (
-    <div className="mt-5 grid space-y-5 lg:mt-10 lg:space-y-20">
-      <SectionShell>
+    <div className="mt-5 grid space-y-5 lg:mt-32 lg:space-y-32">
+      <SectionContainer>
         <Hero />
-      </SectionShell>
+      </SectionContainer>
+      <SectionContainer className="max-lg:hidden">
+        <BrowserWindow link={demoFormLink} hideCopyButton>
+          <div className="min-h-[500px] flex items-center justify-center">
+            <LazyDemoCard />
+          </div>
+        </BrowserWindow>
+      </SectionContainer>
 
       {/* <SectionShell>
         <SectionCard title="What's New">
           <NewFeature />
         </SectionCard>
       </SectionShell> */}
-      <SectionShell className="max-lg:hidden">
+      <SectionContainer className="max-lg:hidden">
         <ScreenshotSlider />
-      </SectionShell>
+      </SectionContainer>
 
-      <SectionShell>
+      <SectionContainer>
         <Features />
-      </SectionShell>
-      <SectionShell>
+      </SectionContainer>
+      <SectionContainer>
         <div className="grid gap-1 lg:grid-cols-2">
           <HowToUseSection />
           <Pricing />
         </div>
-      </SectionShell>
+      </SectionContainer>
       <div className="flex items-center justify-center">
         <Link
           href="https://peerlist.io/growupanand/project/convoform?utm_source=convoform.com"
