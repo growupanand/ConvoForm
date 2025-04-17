49bb74c1ff871b6fad7c090a219e84eaaa56c625
git-workflow-prepare-release <growupanand@gmail.com>
2025-04-17 20:15:55 +0000

    release: changelog for release note v0.17.0

--- packages/release/src/releases/index.ts
+++ packages/release/src/releases/index.ts
@@ -1,9 +1,11 @@
 import { default as Release_0_14_0 } from "./release_0_14_0.mdx";
 import { default as Release_0_15_0 } from "./release_0_15_0.mdx";
 import { default as Release_0_16_0 } from "./release_0_16_0.mdx";
+import { default as Release_0_17_0 } from "./release_0_17_0.mdx";
 
 export const releaseMDXComponents = {
   Release_0_14_0: Release_0_14_0,
   Release_0_15_0: Release_0_15_0,
   Release_0_16_0: Release_0_16_0,
+  Release_0_17_0: Release_0_17_0,
 };
